43803cfabbb6c1fa6e98cc4c57977e630b08c7e5
leauxgan1 <logan.langdon@yahoo.com>
2021-06-11 16:22:28 -0700

    Added Smooth Camera Follow

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform player;
+    public float dampTime = 0.5f;
+    private Vector3 pos;
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = new Vector3(player.position.x, player.position.y,-10.0f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, pos, ref velocity, dampTime);
+    }
+}
